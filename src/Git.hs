b8ce2428c940dfb0c59b195829cd62754792fe87
Roman Timushev <rtimush@gmail.com>
2019-02-18 10:37:32 +0100

    List template branches after cloning

--- src/Git.hs
+++ src/Git.hs
@@ -9,14 +9,20 @@ import           System.Exit
 import           System.Process
 
 newtype RepositoryUrl = RepositoryUrl String
-newtype RepositoryLocation = RepositoryLocation String
 
 class Monad m => MonadGit m where
-    cloneRemoteReporitory :: RepositoryUrl -> RepositoryLocation -> m ()
-
+    cloneRemoteReporitory :: RepositoryUrl -> FilePath -> m ()
+    listBranches :: FilePath -> m [String]
 
 newtype GitCLI a = GitCLI (IO a)
      deriving (Functor, Applicative, Monad, MonadIO, MonadThrow, MonadCatch, MonadMask)
+
+data GitException = GitCallException
+    { exitCode :: Int
+    , stdErr :: String }
+    deriving Show
+
+instance Exception GitException
 
 runGitCLI :: GitCLI a -> IO a
 runGitCLI (GitCLI a) = a
@@ -24,6 +30,22 @@ runGitCLI (GitCLI a) = a
 gitcmd :: [String] -> IO ()
 gitcmd = callProcess "git"
 
+stdoutOrThrow :: MonadThrow m => (ExitCode, String, String) -> m String
+stdoutOrThrow (ExitSuccess, out, err) = return out
+stdoutOrThrow ((ExitFailure code), out, err) =
+    throwM $ GitCallException code err
+
+gitcmdStdout :: (MonadIO m, MonadThrow m) => [String] -> m String
+gitcmdStdout cmd =
+    liftIO $ readProcessWithExitCode "git" cmd "" >>= stdoutOrThrow
+
 instance MonadGit GitCLI where
-    cloneRemoteReporitory (RepositoryUrl url) (RepositoryLocation dst) =
+    cloneRemoteReporitory (RepositoryUrl url) dst =
         liftIO $ gitcmd ["clone", "--no-checkout", url, dst]
+    listBranches repo = fmap lines $ liftIO $ gitcmdStdout
+        [ "-C"
+        , repo
+        , "for-each-ref"
+        , "--format='%(refname:strip=3)'"
+        , "refs/remotes/origin"
+        ]
--- src/Tenpureto.hs
+++ src/Tenpureto.hs
@@ -1,18 +1,37 @@
 module Tenpureto where
 
+import           Data.List
 import           Control.Monad.IO.Class
 import           Control.Monad.Catch
 import           System.IO.Temp
 import           Git
 
+data TemplateBranchConfiguration = TemplateBranchConfiguration
+    { branchName :: String
+    , requiredBranches :: [String]
+    , branchVariables :: [String]
+    }
+
+data ProjectConfiguration = ProjectConfiguration
+    { selectedBranches :: [String]
+    , variableValues :: [(String, String)]
+    }
+
 createProject :: (MonadGit m, MonadIO m, MonadMask m) => String -> Bool -> m ()
 createProject template unattended =
     withSystemTempDirectory "tenpureto" $ prepareTemplate template unattended
 
-updateProject :: (MonadGit m, MonadIO m, MonadMask m) => Maybe String -> Bool -> m ()
+updateProject
+    :: (MonadGit m, MonadIO m, MonadMask m) => Maybe String -> Bool -> m ()
 updateProject template unattended = return ()
 
 prepareTemplate
-    :: (MonadGit m, MonadMask m) => String -> Bool -> FilePath -> m ()
-prepareTemplate template unattended path =
-    cloneRemoteReporitory (RepositoryUrl template) (RepositoryLocation path)
+    :: (MonadGit m, MonadIO m, MonadMask m)
+    => String
+    -> Bool
+    -> FilePath
+    -> m ()
+prepareTemplate template unattended path = do
+    cloneRemoteReporitory (RepositoryUrl template) path
+    branches <- listBranches path
+    liftIO $ putStrLn $ intercalate ", " branches
