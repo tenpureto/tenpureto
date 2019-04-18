96296e427c1135023bb887c3cd09d519db992a1e
Roman Timushev <rtimush@gmail.com>
2019-04-18 17:52:21 +0200

    Use ansi-terminal instead of byline

---
+++ src/Console/Terminal.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Console.Terminal where
+
+import           Control.Monad.Catch
+import           Control.Monad.IO.Class
+import           System.IO                      ( stdout
+                                                , hFlush
+                                                )
+import           Data.Text                      ( Text )
+import qualified Data.Text                     as T
+import           Data.Functor
+import           Data.Text.Prettyprint.Doc
+import           Data.Text.Prettyprint.Doc.Render.Terminal
+import qualified System.Console.Terminal.Size  as TS
+
+data InterruptedException = InterruptedException
+    deriving Show
+
+instance Exception InterruptedException
+
+ask :: MonadIO m => Doc AnsiStyle -> Maybe Text -> m Text
+ask prompt (Just defans) = do
+    say $ prompt <+> annotate (color Black) ((brackets . pretty) defans) <> " "
+    input <- liftIO $ T.pack <$> getLine
+    return $ if T.null input then defans else input
+ask prompt Nothing = do
+    say $ prompt <> " "
+    liftIO $ T.pack <$> getLine
+
+askUntil
+    :: MonadIO m
+    => Doc AnsiStyle
+    -> Maybe Text
+    -> (Text -> Either (Doc AnsiStyle) a)
+    -> m a
+askUntil prompt defans confirm = ask prompt defans <&> confirm >>= reprompt
+  where
+    reprompt (Left  prompt') = askUntil prompt' defans confirm
+    reprompt (Right result ) = return result
+
+say :: MonadIO m => Doc AnsiStyle -> m ()
+say message = liftIO $ do
+    width <- fmap TS.width <$> TS.size
+    let layoutOptions =
+            LayoutOptions (maybe Unbounded (flip AvailablePerLine 1.0) width)
+        render = renderIO stdout . layoutPretty layoutOptions
+    render message
+    hFlush stdout
+
+sayLn :: MonadIO m => Doc AnsiStyle -> m ()
+sayLn message = say (message <> "\n")
