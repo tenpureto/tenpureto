4b849676026eff7fb9ab6fc7ab9a8e4178fafb46
Roman Timushev <rtimush@gmail.com>
2019-12-25 00:10:01 +0300

    Update npm dependencies

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,10 @@ module.exports = {
   title: "Tenpureto",
   description: "Simple and flexible project templates",
   themeConfig: {
-    nav: [{ text: "Home", link: "/" }, { text: "Guide", link: "/guide/" }],
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" }
+    ],
     sidebar: ["/", "/guide/", "/guide/authoring/"],
     repo: "tenpureto/tenpureto",
     docsDir: "docs",
