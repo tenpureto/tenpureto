1434d5d9bc2fa5d5dd46d4b85a67cba28a5d3f6d
Roman Timushev <rtimush@gmail.com>
2020-03-02 09:17:57 +0100

    Generate sitemap for the docs site

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,5 +10,11 @@ module.exports = {
     repo: "tenpureto/tenpureto",
     docsDir: "docs",
     editLinks: true
+  },
+  plugins: {
+    sitemap: {
+      hostname: "https://tenpureto.org",
+      exclude: ["/404.html"]
+    }
   }
 };
