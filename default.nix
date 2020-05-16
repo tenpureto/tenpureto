4a6a50d5fed49ba36653960e3855f38e89a6be2d
Roman Timushev <rtimush@gmail.com>
2020-05-17 01:41:20 +0200

    Update to polysemy 1.3.0.0

--- default.nix
+++ default.nix
@@ -1,78 +1,75 @@
 let
   haskellNix = import (builtins.fetchTarball
-    "https://github.com/input-output-hk/haskell.nix/archive/788e1983768cd810d94e77f4032288fb26e486d4.tar.gz")
+    "https://github.com/input-output-hk/haskell.nix/archive/6ca073db9d236bffeceacd7cfc5a27402b12fd17.tar.gz")
     { };
-  # haskell.nix provides access to the nixpkgs pins which are used by our CI, hence
-  # you will be more likely to get cache hits when using these.
-  # But you can also just use your own, e.g. '<nixpkgs>'
-  nixpkgsSrc = haskellNix.sources.nixpkgs-1909;
-  # haskell.nix provides some arguments to be passed to nixpkgs, including some patches
-  # and also the haskell.nix functionality itself as an overlay.
+  nixpkgsSrc = haskellNix.sources.nixpkgs;
   nixpkgsArgs = haskellNix.nixpkgsArgs;
-in { pkgs ? import nixpkgsSrc nixpkgsArgs }:
-let
-  modules = haskellPackages: [
+
+  pkgs = import nixpkgsSrc nixpkgsArgs;
+  pkgsMusl = pkgs.pkgsCross.musl64;
+
+  modules = [
     { reinstallableLibGhc = true; }
     { packages.ghc.patches = [ ./ghc.patch ]; }
     { packages.terminal-size.patches = [ ./terminal-size.patch ]; }
-    {
+    ({ pkgs, ... }: {
       packages.tenpureto.components.tests.tenpureto-test.build-tools =
-        [ haskellPackages.tasty-discover ];
-    }
+        [ pkgs.haskell-nix.haskellPackages.tasty-discover ];
+    })
     {
       packages.tenpureto.components.tests.tenpureto-test.testWrapper =
         [ "echo" ];
     }
   ];
-  staticCrossPkgs =
-    if pkgs.stdenv.hostPlatform.isLinux then pkgs.pkgsCross.musl64 else pkgs;
 in {
-  default = with pkgs;
-    haskell-nix.stackProject {
-      src = haskell-nix.haskellLib.cleanGit {
-        src = ./.;
-        name = "tenpureto";
-      };
-      modules = modules haskell-nix.haskellPackages;
+  default = pkgs.haskell-nix.stackProject {
+    src = pkgs.haskell-nix.haskellLib.cleanGit {
+      src = ./.;
+      name = "tenpureto";
     };
-  static = with staticCrossPkgs;
-    let
-      libffi-static = libffi.overrideAttrs (oldAttrs: {
-        dontDisableStatic = true;
-        configureFlags = (oldAttrs.configureFlags or [ ])
-          ++ [ "--enable-static" "--disable-shared" ];
-      });
-    in haskell-nix.stackProject {
-      src = haskell-nix.haskellLib.cleanGit {
-        src = ./.;
-        name = "tenpureto";
-      };
-      modules = (modules haskell-nix.haskellPackages) ++ [
-        { doHaddock = false; }
-        {
-          ghc.package =
-            buildPackages.pkgs.haskell-nix.compiler.ghc883.override {
+    modules = modules;
+  };
+  static = let
+    libffi-static = pkgsMusl.libffi.overrideAttrs (oldAttrs: {
+      dontDisableStatic = true;
+      configureFlags = (oldAttrs.configureFlags or [ ])
+        ++ [ "--enable-static" "--disable-shared" ];
+    });
+  in pkgsMusl.haskell-nix.stackProject {
+    src = pkgsMusl.haskell-nix.haskellLib.cleanGit {
+      src = ./.;
+      name = "tenpureto";
+    };
+    modules = modules ++ [
+      { doHaddock = false; }
+      ({ pkgs, ... }:
+        let isMusl = pkgs.buildPackages.targetPlatform.isMusl;
+        in {
+          ghc.package = if isMusl then
+            pkgs.buildPackages.haskell-nix.compiler.ghc883.override {
               enableIntegerSimple = true;
               enableShared = true;
-            };
-        }
-        { packages.ghc.flags.terminfo = false; }
-        { packages.bytestring.flags.integer-simple = true; }
-        { packages.text.flags.integer-simple = true; }
-        { packages.scientific.flags.integer-simple = true; }
-        { packages.integer-logarithms.flags.integer-gmp = false; }
-        { packages.cryptonite.flags.integer-gmp = false; }
-        { packages.hashable.flags.integer-gmp = false; }
-        {
-          packages.tenpureto.components.exes.tenpureto.configureFlags = [
-            "--disable-executable-dynamic"
-            "--disable-shared"
-            "--ghc-option=-optl=-pthread"
-            "--ghc-option=-optl=-static"
-            "--ghc-option=-optl=-L${libffi-static}/lib"
-          ];
-        }
-      ];
-    };
+            }
+          else
+            pkgs.buildPackages.haskell-nix.compiler.ghc883;
+
+          packages.bytestring.flags.integer-simple = isMusl;
+          packages.text.flags.integer-simple = isMusl;
+          packages.scientific.flags.integer-simple = isMusl;
+          packages.integer-logarithms.flags.integer-gmp = !isMusl;
+          packages.cryptonite.flags.integer-gmp = !isMusl;
+          packages.hashable.flags.integer-gmp = !isMusl;
+        })
+      {
+        packages.tenpureto.components.exes.tenpureto.configureFlags = [
+          "--disable-executable-dynamic"
+          "--disable-shared"
+          "--ghc-option=-optl=-pthread"
+          "--ghc-option=-optl=-static"
+          "--ghc-option=-optl=-L${libffi-static}/lib"
+        ];
+      }
+    ];
+  };
   inherit pkgs;
 }
