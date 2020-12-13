3b9f7caf087c026a97331a4fa335234b86225f8e
Roman Timushev <rtimush@gmail.com>
2020-12-13 14:11:21 +0100

    Switch to nix flakes

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "Tenpureto";
+
+  inputs = { haskell.url = "github:input-output-hk/haskell.nix"; };
+
+  outputs = { self, haskell }:
+    let
+
+      project = system:
+        let pkgs = haskell.legacyPackages.${system};
+        in pkgs.haskell-nix.stackProject {
+          src = pkgs.haskell-nix.haskellLib.cleanGit {
+            name = "tenpureto";
+            src = ./.;
+          };
+        };
+
+      drv = system: (project system).tenpureto.components.exes.tenpureto;
+
+      staticProject = system:
+        let pkgs = haskell.legacyPackages.${system};
+        in pkgs.pkgsCross.musl64.haskell-nix.stackProject {
+          src = pkgs.haskell-nix.haskellLib.cleanGit {
+            name = "tenpureto";
+            src = ./.;
+          };
+          modules = [
+            { doHaddock = false; }
+            ({ pkgs, ... }: {
+              ghc.package =
+                pkgs.buildPackages.haskell-nix.compiler.ghc883.override {
+                  enableIntegerSimple = true;
+                }
+              else
+                pkgs.buildPackages.haskell-nix.compiler.ghc884;
+              packages.bytestring.flags.integer-simple = true;
+              packages.text.flags.integer-simple = true;
+              packages.scientific.flags.integer-simple = true;
+              packages.integer-logarithms.flags.integer-gmp = false;
+              packages.cryptonite.flags.integer-gmp = false;
+              packages.hashable.flags.integer-gmp = false;
+            })
+            {
+              packages.tenpureto.components.exes.tenpureto.configureFlags = [
+                "--disable-executable-dynamic"
+                "--disable-shared"
+                "--ghc-option=-optl=-pthread"
+                "--ghc-option=-optl=-static"
+              ];
+            }
+          ];
+        };
+
+      staticDrv = system:
+        (staticProject system).tenpureto.components.exes.tenpureto;
+
+      shell = system:
+        (project system).shellFor {
+          exactDeps = true;
+          withHoogle = true;
+          #tools = { brittany = "0.13.1.0"; };
+        };
+
+    in {
+      packages.x86_64-darwin = { tenpureto = drv "x86_64-darwin"; };
+      packages.x86_64-linux = {
+        tenpureto = drv "x86_64-linux";
+        tenpureto-static = staticDrv "x86_64-linux";
+      };
+
+      defaultPackage.x86_64-darwin = self.packages.x86_64-darwin.tenpureto;
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.tenpureto;
+    };
+
+}
--- shell.nix
+++
@@ -1,4 +0,0 @@
-let hsPkgs = import ./default.nix { };
-in hsPkgs.default.shellFor {
-  buildInputs = with hsPkgs.pkgs.haskellPackages; [ hpack ghcid brittany ];
-}
